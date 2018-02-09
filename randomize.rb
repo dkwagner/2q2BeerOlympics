c5b7403712e7f57416c7001c44211caa1d6f6365
dkwagner <dkwagner@mtu.edu>
2018-02-09 13:36:39 -0500

    New version of script, half way done

---
+++ randomize.rb
@@ -0,0 +1,17 @@
+# Variable declarations
+countries = ["United States", "Canada", "Somolia", "Russia"]
+
+def randomize(numPar)
+  i = 0
+
+
+end
+
+#### main ####
+puts("Please input number of participants:")
+numPar = gets.chomp
+numPar.to_i
+puts "Number of participants: " + numPar
+for i in 1..numPar do
+  
+end
