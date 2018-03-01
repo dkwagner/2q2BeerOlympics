6dd6a65f63a66099b7fcd409bce49a113f24a0dc
dkwagner <dkwagner@mtu.edu>
2018-03-01 08:59:05 -0500

    Finally got around to pushing full version of basic script

--- randomize.rb
+++ randomize.rb
@@ -1,8 +1,30 @@
 # Variable declarations
-countries = ["United States", "Canada", "Somolia", "Russia"]
 
-def randomize(numPar)
-  i = 0
+
+def randomize(numPar, arr)
+  countries = ["United States", "Canada", "Russia", "Korea"]
+  parArr = Array.new(numPar)
+
+  arr.each do |par|
+    loop do
+      temp = rand(numPar)
+      if parArr[temp] == nil
+        parArr[temp] = par
+        break
+      end
+    end
+  end
+
+  puts "Randomized Array: "
+  country = 0
+  parArr.each do |par|
+    puts "\t" + par.to_s + " representing: " + countries[country]
+    country = country + 1
+    if country > 3
+      country = 0
+    end
+  end
+
 
 
 end
@@ -10,8 +32,27 @@ end
 #### main ####
 puts("Please input number of participants:")
 numPar = gets.chomp
-numPar.to_i
 puts "Number of participants: " + numPar
-for i in 1..numPar do
-  
+arr = Array.new(Integer(numPar))
+for i in 0..numPar.to_i - 1 do
+  puts "Enter participant " + (i + 1).to_s + "s name:"
+  temp = gets.chomp
+  arr[i] = temp
+  puts "You have " + (numPar.to_i - (i+1)).to_s + " participants left to enter"
+end
+
+puts "Current participants: "
+arr.each do |par|
+  puts "\t " + par
+end
+
+loop do
+  randomize(numPar.to_i, arr)
+  puts "Are these teams ok? (y/n): "
+  answer = gets.chomp
+  if answer == "y"
+    break
+  else
+    puts "Randomizing teams again..."
+  end
 end
